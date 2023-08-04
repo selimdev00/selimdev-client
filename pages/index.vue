3b5f18d1b998ff9a5be7620ad585d777f75a9ec3
selim <selimdev00@gmail.com>
2023-08-04 16:53:54 +0500

    [try]

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <div
       class="grid lg:grid-cols-[1fr_1.5fr] lg:gap-[100px] items-start relative"
     >
-      <Profile key="profile" v-motion-fade-visible />
+      <!--      <Profile key="profile" v-motion-fade-visible />-->
 
       <!--      <div class="flex flex-col gap-[10px] md:py-10 py-6">-->
       <!--        <ProfileInfo class="lg:hidden block md:py-10 py-4" />-->
